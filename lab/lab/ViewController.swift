bd43c675b3d66b6c1375bb173a623e60fd157c19
thongchai.kk@hotmail.com <banana0895716269>
2014-09-26 16:09:45 +0700

    vcb

---
+++ lab/lab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab
+//
+//  Created by student on 9/26/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mo: UITableView!
+    var 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lab/lab/student.swift
@@ -0,0 +1,17 @@
+//
+//  student.swift
+//  lab
+//
+//  Created by student on 9/26/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import Foundation
+class student (string n,string m, int a)
+{
+   var String name = n
+    var String grade = m
+     var Int sum = a
+
+    
+}
