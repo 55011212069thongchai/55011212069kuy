769c10e6775364196746907ef8ca60efa19153f6
ralintorn.gle@gmail.com <ralintorn0109>
2014-10-17 15:43:04 +0700

    lab/17/10/57

---
+++ lab/navigatoncontro/colorviewtwocontroller.swift
@@ -0,0 +1,38 @@
+//
+//  colorviewtwocontroller.swift
+//  navigatoncontro
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class colorviewtwocontroller: UIViewController {
+
+    @IBOutlet var colorlabel: UILabel!
+    @IBAction func colorselection(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
